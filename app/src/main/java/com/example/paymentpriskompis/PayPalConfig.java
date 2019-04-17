c3b7ba7bafc9bf43a92a2eb0bb7cc7675fd7c26d
Harshi Wanasinghe <wanasinghe.harshi@gmail.com>
2019-04-18 01:22:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/paymentpriskompis/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.example.paymentpriskompis;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AcI06O4MEWd4eNDeH51UKhNgzMJMi5z26HGwQfHHX_l2blHLMtlCxi8quG-uXdR-wSvjFAzTUxKjQZHh";
+}
